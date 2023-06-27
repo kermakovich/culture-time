c20258eeb2b116a842aa7aa805b536164981ef7c
kermakovich <kermakovich.laba@solvd.com>
2023-06-27 16:54:40 +0300

    worked #15

--- src/test/java/integration/neo4j/DancerRepositoryIT.java
+++ src/test/java/integration/neo4j/DancerRepositoryIT.java
@@ -1,7 +1,8 @@
 package integration.neo4j;
 
 import helper.Neo4jBaseIT;
-import org.junit.jupiter.api.Assertions;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
@@ -21,10 +22,15 @@ final class DancerRepositoryIT extends Neo4jBaseIT {
 
     @Test
     void verifiesRightPerformancesCount() {
+        final Long expectedCount = 2L;
         final String dancerId = "b8bc995c-e232-4787-a0f5-1b86d3d6ae9e";
         Long actualCount = dancerRepository
                 .getPerformancesCount(dancerId);
-        Assertions.assertEquals(2L, actualCount, "wrong count");
+        MatcherAssert.assertThat(
+                "performances count",
+                actualCount,
+                Matchers.equalTo(expectedCount)
+        );
     }
 
 }
